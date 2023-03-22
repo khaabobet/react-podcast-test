4c47c3a22d8e79e8226e5a16935eabd90a2c0285
Khalil Aboid Betancort <khalilaboidbetancort@gmail.com>
2023-03-22 18:03:36 +0000

    parser for description implemented

--- src/components/composed/PodcastPlayer.tsx
+++ src/components/composed/PodcastPlayer.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {PodcastEpisode} from "../../models/Podcast/Podcast";
+import parse from 'html-react-parser'
+
 
 interface props {
   episode: PodcastEpisode
@@ -8,7 +10,7 @@ export const PodcastPlayer = (props: props) => {
   return (
     <div className={'podcast-player-container'}>
       <span className={'title'}>{props.episode.title}</span>
-      <span className={'description'}>{props.episode.description}</span>
+      <span className={'description'}>{parse(props.episode.description)}</span>
       <audio src={props.episode.episodeURL} controls>
             Your browser does not support the audio element.
       </audio>
